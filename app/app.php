330607efe34a89876e5ae9065167a5a398d6fb1e
Jonathan Lin <johnlin0214@gmail.com>
2015-03-06 15:18:54 -0500

    Change Phone number button for phone number to type = text to fix the issue of phone number not displaying if the user entered the number with hashes

--- app/app.php
+++ app/app.php
@@ -20,7 +20,7 @@
     });
 
     $app->post("/create_contact", function() use($app){
-        $contact = new Contact($_POST['contact_name'], $_POST['phone_number'], $_POST['address']);
+        $contact = new Contact((string)$_POST['contact_name'], (string)$_POST['phone_number'], (string)$_POST['address']);
         $contact->save();
         return $app['twig']->render('create_contact.twig', array('newContact' => $contact));
     });
