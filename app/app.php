d81fcabc2f274a4727285dc991684b7dacc305a9
Jonathan Lin <johnlin0214@gmail.com>
2015-03-06 14:22:05 -0500

    Fixs the format of the page and adds proper indentation

--- app/app.php
+++ app/app.php
@@ -5,31 +5,30 @@
     session_start();
     if(empty($_SESSION['list_of_contacts']))
     {
-      $_SESSION['list_of_contacts'] = array ();
+        $_SESSION['list_of_contacts'] = array ();
     }
 
     $app = new Silex\Application();
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array (
-           'twig.path'=>__DIR__.'/../views'
+        'twig.path'=>__DIR__.'/../views'
     ));
 
 
     $app->get("/", function() use($app) {
-      return $app['twig']->render('front.twig', array('contacts' => Contact::getAll()));
+        return $app['twig']->render('front.twig', array('contacts' => Contact::getAll()));
     });
 
     $app->post("/create_contact", function() use($app){
-      $contact = new Contact($_POST['contact_name'], $_POST['phone_number'], $_POST['address']);
-      $contact->save();
-      return $app['twig']->render('create_contact.twig', array('newContact' => $contact));
+        $contact = new Contact($_POST['contact_name'], $_POST['phone_number'], $_POST['address']);
+        $contact->save();
+        return $app['twig']->render('create_contact.twig', array('newContact' => $contact));
     });
 
     $app->post("/delete", function() use ($app){
-      Contact::deleteAll();
-      return $app['twig']->render('delete_contact.twig');
+        Contact::deleteAll();
+        return $app['twig']->render('delete_contact.twig');
     });
 
     return $app;
-
 ?>
--- src/contact.php
+++ src/contact.php
@@ -1,54 +1,57 @@
 <?php
-  class Contact
-  {
-    private $name;
-    private $phone_number;
-    private $address;
+    class Contact
+    {
+        private $name;
+        private $phone_number;
+        private $address;
 
+        function __construct($new_name, $new_number, $new_address)
+        {
+            $this->name=$new_name;
+            $this->phone_number=$new_number;
+            $this->address = $new_address;
+        }
 
-    function __construct($new_name, $new_number, $new_address)
-    {
-      $this->name=$new_name;
-      $this->phone_number=$new_number;
-      $this->address = $new_address;
-    }
+        function setAddress($new_address)
+        {
+            $this->address = $new_address;
+        }
 
-    function setAddress($new_address)
-    {
-      $this->address = $new_address;
-    }
-    function getAddress()
-    {
-      return $this->address;
-    }
+        function getAddress()
+        {
+            return $this->address;
+        }
 
-    function setNumber($new_number)
-    {
-      $this->phone_number = $new_number;
-    }
+        function setNumber($new_number)
+        {
+            $this->phone_number = $new_number;
+        }
 
-    function setName($new_name)
-    {
-      $this->name= $new_name;
+        function getNumber()
+        {
+          return $this->phone_number;
+        }
+
+        function setName($new_name)
+        {
+          $this->name= $new_name;
+        }
+        function getName()
+        {
+          return $this->name;
+        }
+
+        static function getAll()
+        {
+          return $_SESSION['list_of_contacts'];
+        }
+        static function deleteALL()
+        {
+          $_SESSION['list_of_contacts'] = array();
+        }
+        function save()
+        {
+          array_push($_SESSION['list_of_contacts'], $this);
+        }
     }
-    function getName()
-    {
-      return $this->name;
-    }
-    function getNumber()
-    {
-      return $this->phone_number;
-    }
-    static function getAll()
-    {
-      return $_SESSION['list_of_contacts'];
-    }
-    static function deleteALL()
-    {
-      $_SESSION['list_of_contacts'] = array ();
-    }
-    function save()
-    {
-      array_push($_SESSION['list_of_contacts'], $this);
-    }
-  }
+?>
